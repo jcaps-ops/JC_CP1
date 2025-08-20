313bc2836f3dafd3ca43f8cdb9ead0b4a64df74f
jcaps-ops <jaxon.castro@ucas-edu.net>
2025-08-20 10:46:42 -0600

    WHYYYYYYYYYYYYYYYYYYYYY well it works well enough

---
+++ le practice/Hello_world.py
@@ -0,0 +1,24 @@
+#Jc 2nd hello world
+namelist = []
+
+Name= input("What is your name child.")
+
+print("welcome" + Name +"That is a nice name " + Name + "  It rolls of the tougue")
+
+namelist.append(Name)
+
+Count = 5
+
+for I in range(0,Count):
+
+    devoisporkpay= input("What is your freinds name child.")
+    I =+ 1
+
+    if devoisporkpay in namelist:
+
+        print("Kid you already told me about that person")
+
+    else:
+
+        print("Thats a cool new freind so bring them to me child")
+
