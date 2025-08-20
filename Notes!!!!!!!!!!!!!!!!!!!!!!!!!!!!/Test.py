e80a60159385a0d8620ac0e0fa5fe36b533dac22
jcaps-ops <jaxon.castro@ucas-edu.net>
2025-08-20 10:26:02 -0600

    did the inputs

--- Notes!!!!!!!!!!!!!!!!!!!!!!!!!!!!/Test.py
+++ Notes!!!!!!!!!!!!!!!!!!!!!!!!!!!!/Test.py
@@ -1,2 +1,18 @@
+#JC 2nd Inputs and outputs notes
+
+
+
+
 print("TeST")
+
+
+
 #added this#
+
+num=input ("  what clor are you sok                                             ")
+if num != "red":
+    print("you sok ar colr "+ num)
+elif num == "red":
+    print("you're sok is fazt")
+elif num == "i have no sok":
+    print("U tru ork boy")
