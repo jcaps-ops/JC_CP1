6ab499480cf39836b4d5ae946a56b6117662c0a8
jcaps-ops <jaxon.castro@ucas-edu.net>
2025-08-21 10:06:31 -0600

    made it so maybe done

---
+++ Starters/lastseason.py
@@ -0,0 +1,5 @@
+#JC 2nd seasons i guess
+
+Season=input("What is your last name child")
+lastname=input("What is your favorite season child")
+print("kid let me get this right your last name is " + Season + " and your favorite season is " + lastname + " oh my goodness are you basic like just shut up")
