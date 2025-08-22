3240bca713b58560f2eafd24ba64d58c9839eb95
jcaps-ops <jaxon.castro@ucas-edu.net>
2025-08-22 10:35:44 -0600

    i forgor the period

--- le practice/who_are_you.py
+++ le practice/who_are_you.py
@@ -2,4 +2,4 @@
 name = input("What is your name user: ")
 age = input("What is your age user: ")
 color = input("What is your favorite color user: ")
-print("your name is " + name + " your age is " + age + " and your favorite color is " + color)
+print("Your name is " + name + " your age is " + age + " and your favorite color is " + color + ".")
