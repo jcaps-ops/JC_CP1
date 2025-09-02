3126265c961cba98caf0fc7d66550755e8387544
jcaps-ops <jaxon.castro@ucas-edu.net>
2025-09-02 10:41:34 -0600

    finished the basic part of it

---
+++ le practice/Madlibs.py
@@ -0,0 +1,9 @@
+#jc 2nd Madlibs
+
+nounein = input("please give me a living thing: ")
+placein = input("please give me a place: ")
+itemein = input("please give me a item: ")
+noundos = input("please give me a new Existance: ")
+nounthree = input("please give me a new Existance: ")
+
+print("it was a stormy night when " + nounein + " Broke into " + placein + ".Now luckly " + nounein + " manged to not trigger any alarms. Then suddenly a fellow robber came to " + placein +" as well the robber being " + noundos + ". then " + nounein + " grabed the " + itemein + " from there back pocket.Then threw the "+ itemein + " at " + noundos + ". Then both of them tripped and fell over and got knocked out. Then in the morning the security guard " + nounthree + " Caught both of them") 
