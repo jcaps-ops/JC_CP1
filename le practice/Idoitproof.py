8e26dc0f6f227da698cef4b1fa4a1e862bbe4178
jcaps-ops <jaxon.castro@ucas-edu.net>
2025-09-04 10:50:25 -0600

    set up

---
+++ le practice/Idoitproof.py
@@ -0,0 +1,8 @@
+#Jc 2nd Idiot proofing
+
+age = input("what is your full name:")
+phone = input("what is your phone#:")
+gpa = float(input("what is your GPA:"))
+
+print("hello" + age + "your phone number is " + phone + "your gpa is" + gpa)
+
