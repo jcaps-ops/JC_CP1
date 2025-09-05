9fe4815aa68243b56561e85de43ebefc9285a40e
jcaps-ops <jaxon.castro@ucas-edu.net>
2025-09-05 10:49:29 -0600

    I have no clue what i am doing

--- Cool projects/coolgame.py
+++ Cool projects/coolgame.py
@@ -1,6 +1,25 @@
 #Jc 2nd Cool Games
 
-print("-1-2-3")
-print("------")
-print("-4-5-6")
-print("------")
+#This is going to be a simple platformer
+pos1 = "-"
+pos2 = "-"
+pos3 = "-"
+pos4 = "-"
+pos5 = "-"
+pos6 = "-"
+pos7 = "-"
+pos8 = "-"
+pos9 = "-"
+pos10 = "-"
+varsection1 = pos1 + pos2 + pos3 + pos4 + pos5 + pos6 + pos7 + pos8 + pos9 + pos10
+print(varsection1)
+listsection1 = ["-","-","-","-","-"]
+filter1 = listsection1
+print(listsection1)
+
+playerx = 0
+playery = 0
+screenlength = len(varsection1)
+print(screenlength)
+if playery == 0:
+    print("test")
