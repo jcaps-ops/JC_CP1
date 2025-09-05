d1403d73d1c0aa09af2296a36eb88c553ad958f5
jcaps-ops <jaxon.castro@ucas-edu.net>
2025-09-05 10:32:07 -0600

    finished it

--- le practice/Idoitproof.py
+++ le practice/Idoitproof.py
@@ -1,8 +1,13 @@
 #Jc 2nd Idiot proofing
 
-age = input("what is your full name:")
+age = input("what is your full name:").strip()
 phone = input("what is your phone#:")
-gpa = float(input("what is your GPA:"))
 
-print("hello" + age + "your phone number is " + phone + "your gpa is" + gpa)
+phonedash = phone.find("-")
+phone = str(phone.replace("-", " "))
+#print(phone)
+
+gpa = float(input("what is your GPA:"))
+roundedgpa = round(gpa, 1)
+print("hello " + age + " your phone number is " + phone + "your gpa is " + str(roundedgpa))
 
