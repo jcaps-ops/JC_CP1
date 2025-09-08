58a8a8950598641b8a717fc4ef2f74c83fca3529
jcaps-ops <jaxon.castro@ucas-edu.net>
2025-09-08 10:51:16 -0600

    setting up three vars lines

--- Cool projects/coolgame.py
+++ Cool projects/coolgame.py
@@ -15,7 +15,7 @@ varsection1 = pos1 + pos2 + pos3 + pos4 + pos5 + pos6 + pos7 + pos8 + pos9 + pos
 print(varsection1)
 listsection1 = ["-","-","-","-","-"]
 filter1 = listsection1
-print(listsection1)
+print("".join(listsection1))
 
 playerx = 0
 playery = 0
@@ -23,3 +23,41 @@ screenlength = len(varsection1)
 print(screenlength)
 if playery == 0:
     print("test")
+
+#No more testing
+
+pos1 = "-"
+pos2 = "-"
+pos3 = "-"
+pos4 = "-"
+pos5 = "-"
+pos6 = "-"
+pos7 = "-"
+pos8 = "-"
+pos9 = "-"
+pos10 = "-"
+pos11 = "-"
+pos12 = "-"
+pos13 = "-"
+pos14 = "-"
+pos15 = "-"
+pos16 = "-"
+pos17 = "-"
+pos18 = "-"
+pos19 = "-"
+pos20 = "-"
+pos21 = "-"
+pos22 = "-"
+pos23 = "-"
+pos24 = "-"
+pos25 = "-"
+pos26 = "-"
+pos27 = "-"
+pos28 = "-"
+pos29 = "-"
+pos30 = "-"
+print("".join(varsection1))
+
+pos1 = "0"
+varsection1 = pos1 + pos2 + pos3 + pos4 + pos5 + pos6 + pos7 + pos8 + pos9 + pos10
+print("".join(varsection1))
