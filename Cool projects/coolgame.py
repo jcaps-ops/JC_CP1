f2a14beb7a5e62291d08098997c59e4123c32e9c
jcaps-ops <jaxon.castro@ucas-edu.net>
2025-09-09 10:09:28 -0600

    set up the thrid line

--- Cool projects/coolgame.py
+++ Cool projects/coolgame.py
@@ -46,18 +46,22 @@ pos17 = "-"
 pos18 = "-"
 pos19 = "-"
 pos20 = "-"
-pos21 = "-"
-pos22 = "-"
-pos23 = "-"
-pos24 = "-"
-pos25 = "-"
-pos26 = "-"
-pos27 = "-"
-pos28 = "-"
-pos29 = "-"
-pos30 = "-"
+pos21 = "^"
+pos22 = "^"
+pos23 = "^"
+pos24 = "^"
+pos25 = "^"
+pos26 = "^"
+pos27 = "^"
+pos28 = "^"
+pos29 = "^"
+pos30 = "^"
 print("".join(varsection1))
 
-pos1 = "0"
+pos11 = "0"
 varsection1 = pos1 + pos2 + pos3 + pos4 + pos5 + pos6 + pos7 + pos8 + pos9 + pos10
+varsection2 = pos11 + pos12 + pos13 + pos14 + pos15 + pos16 + pos17 + pos18 + pos19 + pos20
+varsection3 = pos21 + pos22 + pos23 + pos24 + pos25 + pos26 + pos27 + pos28 + pos29 + pos30
 print("".join(varsection1))
+print("".join(varsection2))
+print("".join(varsection3))
