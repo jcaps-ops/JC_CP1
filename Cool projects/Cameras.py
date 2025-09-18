8478e10615a09d7577ac3816a46562cefd823483
jcaps-ops <jaxon.castro@ucas-edu.net>
2025-09-18 10:51:13 -0600

    started it

---
+++ Cool projects/Cameras.py
@@ -0,0 +1,17 @@
+#Jc 2nd a security system from a horror game
+
+import random
+
+camnum = 1
+currentcam = 1
+
+
+print("Welcome user to the H security cams system for help please type help in your terminal")
+for X in range(1,100):
+    plyaction = input("Terminal input:") 
+    if plyaction == "help":
+        print("For turing on cam systems please enter: Cam activate")
+        print("To Move on cameras please enter: A or D")
+        print("To activate alarms pleas enter:")
+        
+    
