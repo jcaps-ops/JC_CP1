fb33b2a29e37525eb2d6439d1de29444fa73e086
jcaps-ops <jaxon.castro@ucas-edu.net>
2025-09-18 19:44:13 +0000

    Cams are great

--- Cool projects/Cameras.py
+++ Cool projects/Cameras.py
@@ -2,16 +2,45 @@
 
 import random
 
-camnum = 1
 currentcam = 1
+camon = False
+Needsreload = False
+Refreshchance = 1
+plyaction = ""
+
+def breakdown():
+    print("It brok oh")
+    Needsreload = True
+    print(Needsreload)
 
 
 print("Welcome user to the H security cams system for help please type help in your terminal")
 for X in range(1,100):
-    plyaction = input("Terminal input:") 
+    #plyaction = input("Terminal input:") 
     if plyaction == "help":
-        print("For turing on cam systems please enter: Cam activate")
+        print("For turing on cam systems please enter: Cams")
         print("To Move on cameras please enter: A or D")
-        print("To activate alarms pleas enter:")
-        
-    
+        print("To activate alarms please enter:E")
+        print("To refresh please Enter:R")
+        print("To go into tasks Enter:T")
+        print("To Leave cameras please type EXIT")
+    if plyaction == "Cams":
+        camon = False
+        currentcam = 1
+        print("Basic cam layout")
+    if plyaction == "D":
+        if camon == True:
+            currentcam += 1
+            if currentcam > 5:
+                currentcam = 1
+            if currentcam == 2:
+                print("Cam2 layout")
+            if currentcam == 3:
+                print("Cam3 layout")
+            if currentcam == 4:
+                print("Cam4 layout")
+    if plyaction == "T":
+        print("Tasks")
+    if plyaction == "Exit":
+        camon = False
+    breakdown()
