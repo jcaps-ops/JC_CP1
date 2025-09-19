8471cb999a6641b81b9b87c09266cfeba4740630
jcaps-ops <jaxon.castro@ucas-edu.net>
2025-09-19 17:47:36 +0000

    What the

---
+++ le practice/Letter_grade.py
@@ -0,0 +1,27 @@
+#JC 2nd What is my grade
+grade = 97.6
+#grade = input("What is your grade(As a percent)")
+grade = float(grade)
+
+if grade >= 97.5:
+    print("You have an A+")
+elif grade >= 92.5:
+    print("You have an A")
+elif grade >= 90.0:
+    print("You have an A-")
+elif grade >= 87.5:
+    print("You have an b+")
+elif grade >= 82.5:
+    print("You have an B")
+elif grade >= 80.0:
+    print("You have an B-")
+elif grade >= 77.5:
+    print("You have an C+")
+elif grade >= 90.0:
+    print("You have an A-")
+elif grade >= 87.5:
+    print("You have an b+")
+elif grade >= 82.5:
+    print("You have an B")
+elif grade >= 80.0:
+    print("You have an B-")
