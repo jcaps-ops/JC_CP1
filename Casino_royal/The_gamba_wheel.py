58e6fcc613b89459c85da1090208a25de4f15db2
jcaps-ops <jaxon.castro@ucas-edu.net>
2025-09-23 20:20:14 +0000

    started it

---
+++ Casino_royal/The_gamba_wheel.py
@@ -0,0 +1,13 @@
+#JC 2nd this is for a ruletee wheel
+
+money = 100
+bet = 0
+bet_location = "white"
+
+import random
+
+for x in range (1,100):
+    print(f"This is the amount of money you have {money}")
+    p_action = input("How much would you like to bet")
+    bet_location = input("What color are you betting on(black-white-red)")
+
