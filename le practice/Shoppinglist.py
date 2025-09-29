388c9058a864a902fa5991853d385d56846b7dd2
jcaps-ops <jaxon.castro@ucas-edu.net>
2025-09-29 10:50:42 -0600

    stared

---
+++ le practice/Shoppinglist.py
@@ -0,0 +1,9 @@
+#JC 2nd shopping list manager
+shoppinglist = []
+while True:
+    action = input("Would you like to add or remove something from your shopping list (Type add or remove):")
+    #Write your code here
+    if action == "add":
+        adding = input("What would you like to add:")
+
+    
