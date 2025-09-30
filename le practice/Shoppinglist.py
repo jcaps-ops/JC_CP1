c6c1e3d805daf18a092358e68e331807e5f0c3d6
jcaps-ops <jaxon.castro@ucas-edu.net>
2025-09-30 10:06:42 -0600

    finished

--- le practice/Shoppinglist.py
+++ le practice/Shoppinglist.py
@@ -1,9 +1,16 @@
 #JC 2nd shopping list manager
 shoppinglist = []
 while True:
-    action = input("Would you like to add or remove something from your shopping list (Type add or remove):")
+    action = input("Would you like to add or remove something from your shopping list (Type add or remove) type exit to leave or type show to show the list:")
     #Write your code here
     if action == "add":
         adding = input("What would you like to add:")
-
-    
+        shoppinglist.append(adding)
+    if action == "remove":
+        removing = input("What do yo want to remove:")
+        shoppinglist.remove(removing)
+        
+    if action == "exit":
+        break
+    if action == "show":
+        print(shoppinglist)
