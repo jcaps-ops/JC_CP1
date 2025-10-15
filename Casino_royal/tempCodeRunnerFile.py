f9fc08263181dc8107b83bf9dc57c082f725ddae
jcaps-ops <jaxon.castro@ucas-edu.net>
2025-10-15 10:51:14 -0600

    man

--- Casino_royal/tempCodeRunnerFile.py
+++ Casino_royal/tempCodeRunnerFile.py
@@ -1,1 +1,7 @@
-time.sleep(0.1)
+
+    if pathrand == 1:
+        branchpath2 = "dice--------"
+    if pathrand == 2:
+        branchpath2 = "slot machine"
+    if pathrand == 3:
+        branchpath2 = "blackjack---" 
---
+++ Cool projects/Fnaf.py
@@ -0,0 +1,10 @@
+#Fnaf 2nd jc
+
+
+cam1 = ""
+cam2 = ""
+cam3 = ""
+cam4 = ""
+
+while True:
+    setup = input("What do you want to do")
