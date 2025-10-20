86ce119110d809702a3fe78b6363267eae406879
jcaps <jaxon.castro@ucas-edu.net>
2025-10-20 10:51:01 -0600

    har har

---
+++ The best games/hiddenvoice.py
@@ -0,0 +1,24 @@
+#Jc 2nd this is testing a tick system for speed
+import time
+
+tickrate = 0
+
+def tickratetimers(x):
+    for y in range (0, x):
+        time.sleep(0.1)
+        y += 1
+    print("Tick timer")
+
+def clock():
+    global tickrate
+    tickrateinput = input("What speed would you like to do(f, m, s)")
+    if tickrateinput == "f":
+        tickrate = 5
+    elif tickrateinput == "m":
+        tickrate = 10
+    elif tickrateinput == "s":
+        tickrateinput = 15
+while True:
+    tickratetimers(tickrate)
+    playeraction = input("What would you like to do")
+
