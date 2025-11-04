5b5be5e65d6bbf8f5f91e25388da1d808cb8bd88
jcas <jaxon.castro@ucas-edu.net>
2025-11-04 10:49:03 -0700

    I think it works

--- le practice/maze.py
+++ le practice/maze.py
@@ -1,6 +1,7 @@
 #JC 2nd maze games
 import random
 import turtle
+#Store the grid of the maze
 rows = [
       [],
       [],
@@ -20,20 +21,24 @@ colums = [
 wall = turtle.Turtle
 
 def randomize(rows,colums):
+    #Creates the rows of the grid
     for row in rows:
         for x in range (0,6):
             row.append(random.randrange(0,2))
+    #Creates the colums of the grid
     for colum in colums:
         for y in range (0,6):
             colum.append(random.randrange(0,2))
     return(rows,colums)
 def walldraw(rows,colums):
     
+    #Sets up the the turtles
     line1 = turtle.Turtle()
     line1.speed(20000)
     line1.penup()
     line1.setx(-300)
    
+   #Creates the bottom of  the maze
     line1.penup()
     line1.forward(50)
     line1.pensize(5)
@@ -41,18 +46,19 @@ def walldraw(rows,colums):
     line1.sety(0)
     line1.forward(250)
     
-
+    #Creates a wall of  the maze
     line1.pendown()
     line1.left(90)
     line1.forward(50)
     line1.right(180)
     line1.forward(50)
     line1.left(90)
-
+    #Creates a wall of  the maze
     line1.penup()
     line1.setx(-300)
     line1.pendown()
     line1.sety(50)
+    #Based on the rows it draws the walls
     for row in rows:
         for x in row:
             if x == 1:
@@ -76,6 +82,7 @@ def walldraw(rows,colums):
         line1.forward(50)
         line1.right(90)
 
+    #Top of the maze
     line1.penup()
     line1.setx(-300)
     line1.pendown()
@@ -85,12 +92,15 @@ def walldraw(rows,colums):
     line1.penup()
     line1.forward(50)
     
-
+    #Sets up for y
     line1.penup()
     line1.setx(-300)
-    line1.sety(350)
-    line1.right(90)
+    line1.sety(0)
+    line1.right(180)
+    line1.left(90)
     line1.forward(50)
+
+    #Based on the colums it draws
 
     for colum in colums:
         for y in colum:
@@ -114,13 +124,53 @@ def can_solve(rows,colums):
     stack = [(0,0)]
 
     while stack:
-        x, y = stack.pop
+        x, y = stack.pop()
 
-        if x == size - 1 and y == size - 1:
+        if x == size  and y == size:
+            return True
+        if (x, y) in visted:
+            continue
+        visted.add((x,y))
 
+        if x < size and colums[y][x+1] == 0:
+            stack.append((x+ 1, y))
+        if y < size and colums[y+1][x] == 0:
+            stack.append((x, y+1))
+        if x > 0 and colums[y-1][x] == 0:
+            stack.append((x - 1 , y))
+        if y > 0 and colums[y][x-1] == 0:
+            stack.append((x , y - 1))
+    
+    return False
+# Sets up the randomizers
+turncounter = 0
+issolvable = False 
+while issolvable == False:
+    rows = [
+      [],
+      [],
+      [],
+      [],
+      [],
+      []
+    ]
+    colums = [
+      [],
+      [],
+      [],
+      [],
+      [],
+      []
+    ]   
+    randomizer1 = randomize(rows,colums)
+    rows = randomizer1[0]
+    rows = randomizer1[1]
+    solvable = can_solve(rows,colums)
+    turncounter += 1
+    if solvable == True:
+        print(turncounter)
+        issolvable = True
 
-randomizer1 = randomize(rows,colums)
-rows = randomizer1[0]
-rows = randomizer1[1]
+print("It can be solved")    
 walldraw(rows,colums)
 turtle.done()
