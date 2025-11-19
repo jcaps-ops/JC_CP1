c89dd3b8363d8cb035b132bbc69e7ad1053191ca
jcaps-ops <jaxon.castro@ucas-edu.net>
2025-11-19 10:03:28 -0700

    finsihbed it

--- le practice/flexablecalculator.py
+++ le practice/flexablecalculator.py
@@ -1,11 +1,20 @@
 #jc 2nd flexable calculator
 
+import math
+
 def calc(want, *numbers):
    if want == "1":
        revalue = sum(numbers[0])
    if want == "2":
        valuetotal = sum(numbers[0])
        revalue = valuetotal/ len(numbers[0])
+   if want == "3":
+       revalue = max(numbers[0])
+   if want == "4":
+       revalue = min(numbers[0])
+   if want == "5":
+       revalue = math.prod(numbers[0])
+    
    return revalue
 
 def wants():
@@ -17,7 +26,7 @@ testlist = []
 while True:
     numimput = input("what number to add(N to stop)")
     #isnumeric
-    if numimput.isdigit():
+    if numimput.isnumeric():
         numimput = float(numimput)
         testlist.append(numimput)
     if numimput == "n":
