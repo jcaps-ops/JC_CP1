534d7f32848240a3d309b5b0d97f580e3591043b
jcaps-ops <jaxon.castro@ucas-edu.net>
2025-11-25 10:51:42 -0700

    god of Puesdo code i beg of the i am willing to sacrifice my seat partner for better puedsocode

---
+++ projects/Thefinalproject.py
@@ -0,0 +1,80 @@
+#Jc 2nd final project
+
+#def function combat(php,pd,pmd,ps,pm,mhp,md,mmd,ms,spell list,mn)
+#if ps > ms:
+#turn = p
+#else
+#turn = m
+#while fighting
+    #if turn = p:
+        #while turn
+            #input player action between 1 and 2
+            #if player action == 1:
+                #list player options between basic attacks,defnse and speed boost,and sword swipe via 1-4 options
+                #if player action == 1:
+                    #Attack doing the sword damage
+                    #if sword == weak sword:
+                        #Damage = radnom between 3-5 damage
+                    #if sword == sword:
+                        #Damage = radnom between 5-6 damage
+                    #set as physical damage
+                #elif player action == 2:
+                    #increase defense for the battle
+                    #pd += 2
+                #elif player action == 3:
+                    #increase speed for the battle
+                    #ps += 2
+                #elif player action == 4 and has flintlock:
+                    #increase speed for the battle
+                    #Damage = radnom between 4-8 damage
+                    #ps -= 1
+                #else
+                    #display that is not an option
+                    #Continue to the top 
+            #elif player action == 2:
+                #Then list players options asking play to give the spells name
+                #The the player imput for spell
+                #if spell input == heal and has this is in spell list :
+                    #php adds 3
+                    #mana subtract 2
+                #elif spell input == fire blade and has this is in spell list :
+                    #Damage = random between 4-6 damage
+                    #mana sumbrtact 2
+                    #set as magical damage
+                #elif spell input == ice spike and has this is in spell list :
+                    #Damage = random between 7-8 damage
+                    #mana subrtact 4
+                    #set as magical damage
+                #else
+                    #display that is not an option
+                    #Continue to the top 
+            #else
+                #display that is not an option
+                #Continue to the top 
+            #turn is false
+    #else
+        #if mn == ratman
+                #damage = 1-3 
+                #set damage as physical
+                #display the ratman swiped at you
+        #if mn == cultist
+                #ma = 1-2
+                #if ma = 1
+                    #damage = 2-6 
+                    #set damage as physical
+                    #display the cultist stabed you at you
+                #if ma = 2
+                    #damage = 3-5 
+                    #set damage as magical
+                    #display the cultist used a mental attack on you
+        
+
+#
+
+
+#
+
+
+
+#Set up the starting stats for the player and the game
+#room = "te"
